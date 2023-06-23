7fcb4e791f222e02f5ccc3f01ebf00b85a693905
piettro <piettroenrico@hotmail.com>
2023-06-23 19:14:05 -0300

    Aula 3

--- conta.py
+++ conta.py
@@ -1,6 +1,5 @@
 class Conta ():
-    
-    
+
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto... ")
         self.numero = numero
@@ -8,3 +7,13 @@ class Conta ():
         self.saldo = saldo
         self.limite = limite
 
+    def extrato(self):
+        print(f"Saldo {self.saldo} do titular {self.titular}")
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
+
+    
--- test.py
+++ test.py
@@ -1,3 +1,29 @@
 from conta import Conta
 
+##Criando conta 1
 conta = Conta(123,"Piettro",50, 100)
+
+extrato = conta.extrato()
+print(extrato)
+
+conta.deposita(50)
+extrato = conta.extrato()
+print(extrato)
+
+conta.saca(100)
+extrato = conta.extrato()
+print(extrato)
+
+##Criando conta 2
+conta2 = Conta(222,"Pedro",100, 200)
+
+extrato2 = conta2.extrato()
+print(extrato)
+
+conta.deposita(70)
+extrato2 = conta2.extrato()
+print(extrato)
+
+conta.saca(65.5)
+extrato2 = conta2.extrato()
+print(extrato)
